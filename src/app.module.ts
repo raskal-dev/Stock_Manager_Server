53e6f83a8d7552062c7f30adf832d494e0789ee4
raskal-dev <kalvinrasamyzo@gmail.com>
2025-04-06 20:45:16 +0300

    feat(User): create Entity done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/users/entities/user.entity';
 
+// @Module({
+//   imports: [UsersModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'agrohelp',
+      password: 'agrohelp',
+      database: 'agrohelp_db',
+      entities: [
+        User,
+      ],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Role } from "src/utils/role.enum";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn({ type: "int" })
+    id: number;
+
+    @Column({ type: "varchar", length: 50, nullable: true })
+    name: string;
+
+    @Column({ type: "varchar", length: 30, nullable: true })
+    phone: string;
+
+    @Column({ type: "varchar", length: 50, nullable: false, unique: true })
+    email: string;
+
+    @Column({ type: "text", nullable: false })
+    password: string;
+
+    @Column({ type: "varchar", length: 10, nullable: false })
+    role: Role;
+
+    @Column({ type: "boolean", default: false })
+    isActive: boolean;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt: Date;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updatedAt: Date;
+}

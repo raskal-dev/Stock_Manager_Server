3c6da205768fac3fb68a7f05e9127a3b9c3d103e
raskal-dev <kalvinrasamyzo@gmail.com>
2025-05-03 11:28:16 +0300

    feat(): update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/users/entities/user.entity';
 
-// @Module({
-//   imports: [UsersModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
 @Module({
   imports: [
     TypeOrmModule.forRoot({

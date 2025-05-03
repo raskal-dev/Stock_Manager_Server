2ee3b8fe930ad63af49af537fd2af825cf8634a7
raskal-dev <kalvinrasamyzo@gmail.com>
2025-05-03 11:26:11 +0300

    feat(): add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { User } from './modules/users/entities/user.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'agrohelp',
-      password: 'agrohelp',
-      database: 'agrohelp_db',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [
         User,
       ],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Stock Manager Server')
+    .setDescription('OpenAPI for Stock Manager Server API')
+    .setVersion('1.0')
+    .addTag('Stock Manager')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
